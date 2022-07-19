fc41682662f5c880a77f1d483537b1c3ea1da54e
ziyodbek0300 <mr.prgrmr03@gmail.com>
2022-07-19 16:21:31 +0500

    get all users

--- index.js
+++ index.js
@@ -7,40 +7,46 @@ const mongoose = require('mongoose')
 const userSchema = require('./schema')
 const User = mongoose.model('user', userSchema, 'user')
 
-async function createUser(name, age, gender, date) {
-    try {
-        await mongoose.connect(process.env.connectionString);
-        return new User({
-            name,
-            age,
-            gender,
-            date,
-            created: Date.now()
-        }).save()
-    } catch (e) {
+// async function createUser(name, age, gender, date) {
+//     try {
+//         await mongoose.connect(process.env.connectionString);
+//         return new User({
+//             name,
+//             age,
+//             gender,
+//             date,
+//             created: Date.now()
+//         }).save()
+//     } catch (e) {
 
-    }
-}
+//     }
+// }
 
-app.get('/', async (req, res) => {
-    try {
-        await createUser("Adam", 20, "Male", new Date());
-        res.send("ok");
-    } catch (e) {
-        res.send(e.message);
-    }
+mongoose
+    .connect(process.env.connectionString)
+    .then((x) => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    })
+    .catch((err) => {
+        console.error('Error connecting to mongo', err.reason)
+    })
+
+
+app.get('/users', async (req, res) => {
+    const all = await User.find();
+    res.send(all)
 })
 
-app.get('/about', async (req, res) => {
-    console.log("asdfsd");
-    try {
-        await User.find({}).then(u => {
-            console.log(u);
-        })
-    } catch(e) {
-        console.log(e);
-    }
-    res.send({message: "Hello"});
-})
+// app.get('/about', async (req, res) => {
+//     console.log("asdfsd");
+//     try {
+//         await User.find({}).then(u => {
+//             console.log(u);
+//         })
+//     } catch (e) {
+//         console.log(e);
+//     }
+//     res.send({ message: "Hello" });
+// })
 
 app.listen(5000, () => console.log("Server is running..."))
